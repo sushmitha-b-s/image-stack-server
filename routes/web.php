95936dca680ce5dd9600006bb38ac428a74c87e7
sushmitha-b-s <bssushmitha26@gmail.com>
2021-06-16 16:16:12 +0200

    created images controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ImagesController::class, 'index']);
