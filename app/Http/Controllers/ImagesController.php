b6562c028164ed899eb5799040224047b876a3ef
sushmitha-b-s <bssushmitha26@gmail.com>
2021-06-16 16:51:26 +0200

    created the controller and the migration for image

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Image;
 
 class ImagesController extends Controller
 {
     public function index() {
-        return 'index method';
+        return response()->json(Image::all(), 200);
     }
 }
